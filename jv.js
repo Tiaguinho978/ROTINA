fc670082c1df9c18c6eb4a5f66268e8f0bd44962
Tiago Reckziegel Hilgemberg <113403244+Tiaguinho978@users.noreply.github.com>
2023-03-04 03:56:13 -0300

    Add files via upload

---
+++ jv.js
@@ -0,0 +1,23 @@
+$("#list1 #b").on("click", function (event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $("html, body").animate(
+            {
+                scrollTop: $(hash).offset().top - 100
+            },
+            800
+        );
+    }
+});
+
+// Sticky menu background
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 150) {
+        document.querySelector("#list1").style.opacity = 0.9;
+    } else {
+        document.querySelector("#list1").style.opacity = 1;
+    }
+});
